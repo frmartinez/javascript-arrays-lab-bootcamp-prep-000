073aa1580e1ef92b343f62ba7ed33616728eaead
Fernando Martinez <martinezfer@gmail.com>
2019-01-19 22:41:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ kittens.push(name) ;
 return kittens ;
 } 
 
-kittens = ['Milo', 'Otis', 'Garfield']
+kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyPrependKitten(name){ 
 kittens.unshift(name) ;
 return kittens ;
 } 
 
-kittens = ['Milo', 'Otis', 'Garfield']
+kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyRemoveLastKitten(name){ 
 kittens.shift(name) ;
 return kittens ;
